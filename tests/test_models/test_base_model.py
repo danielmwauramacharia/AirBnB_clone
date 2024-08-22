ba7832a559d8aaa171db957388dd54ddd6fe634d
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-22 19:24:02 +0300

    update the base_model test file, 'testing the to dictionary method'

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,11 +3,21 @@
 import unittest
 from datetime import datetime
 import time
+from uuid import uuid4
 from models.base_model import BaseModel
 
 
 class TestBase(unittest.TestCase):
     """Test case for the basemodel"""
+
+    def test_initialization(self):
+        """Test the __init__ method"""
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+        self.assertEqual(str(type(model)),
+                         "<class 'models.base_model.BaseModel'>")
 
     def test_save(self):
         """Testing the method save in base_model module"""
@@ -17,6 +27,16 @@ class TestBase(unittest.TestCase):
         test.save()
         diff = test.updated_at - now
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
+
+    def test_to_dict(self):
+        """Test the to_dict method"""
+        dict_inst = BaseModel()
+        to_dict = dict_inst.to_dict()
+        self.assertIsInstance(to_dict, dict)
+        self.assertIn("id", to_dict.keys())
+        self.assertIn("created_at", to_dict.keys())
+        self.assertIn("updated_at", to_dict.keys())
+        self.assertEqual(to_dict['__class__'], type(to_dict).__name__)
 
 
 if __name__ == "__main__":
